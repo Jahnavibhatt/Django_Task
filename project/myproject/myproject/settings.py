5db3061dea90035801be389ed31e20bc09dd9233
Jahnavibhatt <42162752+Jahnavibhatt@users.noreply.github.com>
2020-06-30 18:23:48 +0530

    Update settings.py

--- project/myproject/myproject/settings.py
+++ project/myproject/myproject/settings.py
@@ -126,8 +126,8 @@ STATICFILES_DIRS = [
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'janvibhatt1896@gmail.com'
-EMAIL_HOST_PASSWORD = 'jbfb*9631EU'
+EMAIL_HOST_USER = 'janvibhatt@gmail.com'
+EMAIL_HOST_PASSWORD = '***********'
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = 'Janvi <Janvibhatt1896@gmail.com>'
 
